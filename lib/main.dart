0fa275a8fbac36aa49207e24f4f21902d822895d
lavanyaRamesh064 <lavanyaramesh2903@gmail.com>
2024-02-01 00:08:57 +0530

    Included array of icons and questions

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(Quizzler());
-}
+void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
-  const Quizzler({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.grey.shade900,
-        body: const SafeArea(
+        body: SafeArea(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: Quiz(),
+            child: QuizPage(),
           ),
         ),
       ),
@@ -23,16 +19,110 @@ class Quizzler extends StatelessWidget {
   }
 }
 
-class Quiz extends StatefulWidget {
-  const Quiz({Key? key}) : super(key: key);
-
+class QuizPage extends StatefulWidget {
   @override
-  State<Quiz> createState() => _QuizState();
+  _QuizPageState createState() => _QuizPageState();
 }
 
-class _QuizState extends State<Quiz> {
+class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreIcons = [
+    const Icon(
+      Icons.close,
+      color: Colors.green,
+    ),
+    const Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+    const Icon(
+      Icons.close,
+      color: Colors.red,
+    ),
+    const Icon(
+      Icons.close,
+      color: Colors.red,
+    )
+  ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        const Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                'This is where the question text will go.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.green,
+              ),
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  scoreIcons.add(
+                    Icon(Icons.check, color: Colors.green),
+                  );
+                });
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: Colors.red,
+              ),
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  scoreIcons.add(
+                    Icon(Icons.close, color: Colors.red),
+                  );
+                });
+              },
+            ),
+          ),
+        ),
+        Row(
+          children: scoreIcons,
+        )
+      ],
+    );
   }
 }
+
+/*
+question1: 'You can lead a cow down stairs but not up stairs.', false,
+question2: 'Approximately one quarter of human bones are in the feet.', true,
+question3: 'A slug\'s blood is green.', true,
+*/
